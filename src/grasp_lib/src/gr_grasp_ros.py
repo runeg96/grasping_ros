688d10a37573cc708b0f3174b1f7f396b767f891
Rune <runeg96@hotmail.com>
2021-03-11 15:58:03 +0100

    Updated gr_grasp_ros (kinda working)

--- src/grasp_lib/src/gr_grasp_ros.py
+++ src/grasp_lib/src/gr_grasp_ros.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import os
 import sys
@@ -16,29 +16,30 @@ import torch.utils.data
 
 from sensor_msgs.msg import Image
 
-from hardware.camera import RealSenseCamera
+# from hardware.camera import RealSenseCamera
 from hardware.device import get_device
 from inference.post_process import post_process_output
 from utils.data.camera_data import CameraData
 from utils.visualisation.plot import save_results, plot_results
+from utils.dataset_processing.grasp import detect_grasps
 
 logging.basicConfig(level=logging.INFO)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Evaluate network')
-    parser.add_argument('--network', type=str, default='saved_data/cornell_rgbd_iou_0.96',
+    parser.add_argument('--network', type=str, default='../resources/epoch_17_iou_0.96',
                         help='Path to saved network to evaluate')
     parser.add_argument('--use-depth', type=int, default=1,
                         help='Use Depth image for evaluation (1/0)')
     parser.add_argument('--use-rgb', type=int, default=1,
                         help='Use RGB image for evaluation (1/0)')
-    parser.add_argument('--n-grasps', type=int, default=1,
+    parser.add_argument('--n-grasps', type=int, default=10,
                         help='Number of grasps to consider per image')
     parser.add_argument('--cpu', dest='force_cpu', action='store_true', default=False,
                         help='Force code to run in CPU mode')
-
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
+    # args = parser.parse_args()
     return args
 
 def numpy_to_torch(s):
@@ -46,6 +47,14 @@ def numpy_to_torch(s):
         return torch.from_numpy(np.expand_dims(s, 0).astype(np.float32))
     else:
         return torch.from_numpy(s.astype(np.float32))
+
+def normalise(img):
+    """
+    Normalise the image by converting to float [0,1] and zero-centering
+    """
+    img = img.astype(np.float32) / 255.0
+    img -= img.mean()
+    return img
 
 if __name__ == '__main__':
     args = parse_args()
@@ -74,30 +83,34 @@ if __name__ == '__main__':
             # x, depth_img, rgb_img = cam_data.get_data(rgb=rgb, depth=depth)
 
             color_msg = rospy.wait_for_message('ptu_camera/camera/color/image_raw', Image, timeout=rospy.Duration(1))
-            rgb_img = np.frombuffer(color_msg.data, dtype=np.uint8).reshape(color_msg.height, color_msg.width, -1)
+            rgb_image = np.frombuffer(color_msg.data, dtype=np.uint8).reshape(color_msg.height, color_msg.width, -1)
+            rgb_img = normalise(rgb_image)
 
             depth_msg = rospy.wait_for_message('ptu_camera/camera/aligned_depth_to_color/image_raw', Image, timeout=rospy.Duration(1))
-            depth_img = np.frombuffer(depth_msg.data, dtype=np.uint16).reshape(depth_msg.height, depth_msg.width)
-            
-            x =  numpy_to_torch(np.concatenate((np.expand_dims(depth_img, 0), np.expand_dims(rgb_img, 0)), 1))
+            depth_image = np.frombuffer(depth_msg.data, dtype=np.uint16).reshape(depth_msg.height, depth_msg.width, -1)
+            depth_img = normalise(depth_image)
+
+            x =  numpy_to_torch(np.concatenate((np.expand_dims(depth_img.transpose(2,0,1), 0), np.expand_dims(rgb_img.transpose(2,0,1), 0)), 1))
 
             with torch.no_grad():
                 xc = x.to(device)
                 pred = net.predict(xc)
 
                 q_img, ang_img, width_img = post_process_output(pred['pos'], pred['cos'], pred['sin'], pred['width'])
-
+                grasps = detect_grasps(q_img, ang_img, width_img)
+                print(grasps[0].center[0],grasps[0].center[1])
+                
                 plot_results(fig=fig,
-                             rgb_img=rgb_img,
-                             depth_img=depth_img,
+                             rgb_img=rgb_image,
+                             depth_img=depth_image,
                              grasp_q_img=q_img,
                              grasp_angle_img=ang_img,
                              no_grasps=args.n_grasps,
                              grasp_width_img=width_img)
     finally:
         save_results(
-            rgb_img=rgb_img,
-            depth_img=depth_img,
+            rgb_img=rgb_image,
+            depth_img=depth_image,
             grasp_q_img=q_img,
             grasp_angle_img=ang_img,
             no_grasps=args.n_grasps,
