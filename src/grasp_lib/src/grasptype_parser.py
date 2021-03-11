91ab54340846469759ba6a5bd46dd5ffbb311877
Rune Grønhøj <runeg96@hotmail.com>
2021-03-11 11:08:12 +0100

    Fixed grasp parser

--- src/grasp_lib/src/grasptype_parser.py
+++ src/grasp_lib/src/grasptype_parser.py
@@ -13,25 +13,32 @@ def parser_callback(msg):
     '''
     ggcnn: msg.data = [x, y, z, ang, width, depth_center]
     '''
-    
+
     new_grasp = Grasp()
 
     new_grasp.pose.position.x = msg.data[0]
     new_grasp.pose.position.y = msg.data[1]
     new_grasp.pose.position.z = msg.data[2]
 
-    new_grasp.pose.orientation = quaternion_from_euler(0.0, 0.0, msg.data[3])
+    
+    q = quaternion_from_euler(0.0, 0.0, msg.data[3])
+    new_grasp.pose.orientation.x = q[0]
+    new_grasp.pose.orientation.y = q[1]
+    new_grasp.pose.orientation.z = q[2]
+    new_grasp.pose.orientation.w = q[3]
 
-    new_grasp.width = msg.data[4]
+
+    new_grasp.width = msg.data[4]/1000
 
     new_grasp.quality = -1.0 # Unknown
-
+    print(new_grasp)
     grasp_pub.publish(new_grasp)
 
 
 
 rospy.init_node("grasptype_parser_node")
 
-grasp_pub = rospy.Publisher(rospy.get_param('visualize_grasp/input/grasp_topic'), Grasp, queue_size=1)
+grasp_pub = rospy.Publisher(rospy.get_param("visualize_grasp/input/grasp_topic"), Grasp, queue_size=1)
 
 rospy.Subscriber('ggcnn/out/command', Float32MultiArray, parser_callback)
+rospy.spin()
