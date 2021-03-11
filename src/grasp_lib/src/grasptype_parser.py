0cc2087d1987b14172ea9a5b73899fc759e30270
Jan <jkja16@student.aau.dk>
2021-03-11 10:41:53 +0100

    Added test grasp parser ggcnn

---
+++ src/grasp_lib/src/grasptype_parser.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+
+import rospy
+
+from tf.transformations import quaternion_from_euler
+
+from std_msgs.msg import Float32MultiArray
+from ggcnn.msg import Grasp
+
+
+def parser_callback(msg):
+    '''
+    ggcnn: msg.data = [x, y, z, ang, width, depth_center]
+    '''
+    
+    new_grasp = Grasp()
+
+    new_grasp.pose.position.x = msg.data[0]
+    new_grasp.pose.position.y = msg.data[1]
+    new_grasp.pose.position.z = msg.data[2]
+
+    new_grasp.pose.orientation = quaternion_from_euler(0.0, 0.0, msg.data[3])
+
+    new_grasp.width = msg.data[4]
+
+    new_grasp.quality = -1.0 # Unknown
+
+    grasp_pub.publish(new_grasp)
+
+
+
+rospy.init_node("grasptype_parser_node")
+
+grasp_pub = rospy.Publisher(rospy.get_param('visualize_grasp/input/grasp_topic'), Grasp, queue_size=1)
+
+rospy.Subscriber('ggcnn/out/command', Float32MultiArray, parser_callback)
