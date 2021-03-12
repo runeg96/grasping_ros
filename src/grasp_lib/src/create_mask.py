fb2c28b518c030fa722b667d7c6444d78a3b95e3
Rune <runeg96@hotmail.com>
2021-03-12 14:55:37 +0100

    Create mask 640 x 480

--- src/grasp_lib/src/create_mask.py
+++ src/grasp_lib/src/create_mask.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2 as cv
 from PIL import Image
 
-img = np.ones((720,1280))
-cv.imwrite("workspace_mask_1280.png",img)
-workspace_mask = np.array(Image.open("/home/slave/Documents/workspaces/handover_ws/workspace_mask_1280.png"),dtype=bool)
-print(workspace_mask)
-
-workspace_mask = np.array(Image.open("/home/slave/Documents/workspaces/handover_ws/src/lh7-handover/grasping_ros/src/graspnet_baseline/doc/example_data/workspace_mask.png"))
-print(workspace_mask)
+img = np.ones((480,640))
+cv.imwrite("workspace_mask_640.png",img)
+# workspace_mask = np.array(Image.open("/home/slave/Documents/workspaces/handover_ws/workspace_mask_1280.png"),dtype=bool)
+# print(workspace_mask)
+#
+# workspace_mask = np.array(Image.open("/home/slave/Documents/workspaces/handover_ws/src/lh7-handover/grasping_ros/src/graspnet_baseline/doc/example_data/workspace_mask.png"))
+# print(workspace_mask)
