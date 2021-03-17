7fcc662f557623cac01549fd8a102ff9b152c58b
Jan <jkja16@student.aau.dk>
2021-03-17 16:08:02 +0100

    Added docstring and cleaned old comments

--- src/grasp_lib/src/grasp_utils/utils.py
+++ src/grasp_lib/src/grasp_utils/utils.py
@@ -5,30 +5,38 @@ import pyrealsense2 as rs
 FOCAL_D345 = 1.93 #mm
 
 
-def width_pixel_to_m(width_pixel, depth_m, cam_info):
-    fovx = 2 * math.atan(cam_info.width / (2 * cam_info.K[0]))
-    return (2 * depth_m * math.tan(fovx/2)) / cam_info.width * width_pixel
+def width_pixel_to_m(width_pixel, depth_m, cameraInfo):
+    """Convert pixel measure to meters
 
-def width_m_to_pixel(width_m, depth_m, cam_info):
-    fovx = 2 * math.atan(cam_info.width / (2 * cam_info.K[0]))
-    return (width_m * cam_info.width) / (2 * depth_m * math.tan(fovx/2)) 
+    Keyword arguments:
+    width_pixel -- measurement in pixels
+    depth_m     -- float depth in meters
+    cameraInfo  -- sensor_msgs/CameraInfo
+    """
+    
+    fovx = 2 * math.atan(cameraInfo.width / (2 * cameraInfo.K[0]))
+    return (2 * depth_m * math.tan(fovx/2)) / cameraInfo.width * width_pixel
 
-def new_width_pixel_to_m(width_pixel, center, angle, cam_info):
+def width_m_to_pixel(width_m, depth_m, cameraInfo):
+    """Convert meter measure to pixels
+
+    Keyword arguments:
+    width_m     -- float measurement in meters
+    depth_m     -- float depth in meters
+    cameraInfo  -- sensor_msgs/CameraInfo
+    """
+
+    fovx = 2 * math.atan(cameraInfo.width / (2 * cameraInfo.K[0]))
+    return (width_m * cameraInfo.width) / (2 * depth_m * math.tan(fovx/2)) 
+
+def old_width_pixel_to_m(width_pixel, center, angle, cameraInfo):
     p1 = [center[0]-math.cos(angle)*width_pixel, center[1]-math.sin(angle)*width_pixel]
     p2 = [center[0]+math.cos(angle)*width_pixel, center[1]+math.sin(angle)*width_pixel]
 
-    P1 = pixel_to_camera(cam_info, p1)
-    P2 = pixel_to_camera(cam_info, p2)
+    P1 = pixel_to_camera(cameraInfo, p1)
+    P2 = pixel_to_camera(cameraInfo, p2)
 
     return math.sqrt((P2[0] - P1[0])**2 + (P2[1] - P1[1])**2 + (P2[2] - P1[2])**2)
-
-
-# def width_m_to_pixel(width_m, depth_m):
-#     return (width_m * (FOCAL_D345/1000)) / depth_m
-
-# def width_pixel_to_m(width_pixel, depth_m):
-#     return (width_pixel * depth_m) / (FOCAL_D345/1000)
-
 
 def cam_to_world(cam_point, world_to_cam):
     """Convert from camera_frame to world_frame
@@ -37,7 +45,6 @@ def cam_to_world(cam_point, world_to_cam):
     cam_pose   -- PoseStamped from camera view
     cam_frame  -- The frame id of the camera
     """
-    # cam_point = np.array([cam_pose[0], cam_pose[1], cam_pose[2]])
 
     obj_vector = np.concatenate((cam_point, np.ones(1))).reshape((4, 1))
     world_point = np.dot(world_to_cam, obj_vector)
@@ -46,6 +53,13 @@ def cam_to_world(cam_point, world_to_cam):
     return world_point[0:3]
 
 def pixel_to_camera(cameraInfo, pixel, depth):
+    """Deproject 2D pixel to 3D point
+
+    Keyword arguments:
+    cameraInfo  -- sensor_msgs/CameraInfo
+    pixel       -- int tuple pixel to be deprojected
+    depth       -- float depth value at pixel
+    """
     _intrinsics = rs.intrinsics()
     _intrinsics.width = cameraInfo.width
     _intrinsics.height = cameraInfo.height
@@ -53,22 +67,21 @@ def pixel_to_camera(cameraInfo, pixel, depth):
     _intrinsics.ppy = cameraInfo.K[5]
     _intrinsics.fx = cameraInfo.K[0]
     _intrinsics.fy = cameraInfo.K[4]
-    #_intrinsics.model = cameraInfo.distortion_model
+    
     _intrinsics.model  = rs.distortion.none
     _intrinsics.coeffs = [i for i in cameraInfo.D]
     x,y = pixel
     result = rs.rs2_deproject_pixel_to_point(_intrinsics, [x,y], depth)
-    # return rs.rs2_deproject_pixel_to_point(depth_intrin, pixel, 1.0)
-    # x = result[0]
-    # y = result[1]
-    # depth = result[2]
-    # z_squared = depth**2 - x**2 - y**2
-    # z = np.sqrt(z_squared)
-    # result[2] = z
 
     return result
 
 def camera_to_pixel(cameraInfo, point):
+    """Project 3D point to 2D pixel
+
+    Keyword arguments:
+    cameraInfo  -- sensor_msgs/CameraInfo
+    point       -- float array of 3D point
+    """
     _intrinsics = rs.intrinsics()
     _intrinsics.width = cameraInfo.width
     _intrinsics.height = cameraInfo.height
@@ -76,7 +89,7 @@ def camera_to_pixel(cameraInfo, point):
     _intrinsics.ppy = cameraInfo.K[5]
     _intrinsics.fx = cameraInfo.K[0]
     _intrinsics.fy = cameraInfo.K[4]
-    #_intrinsics.model = cameraInfo.distortion_model
+    
     _intrinsics.model  = rs.distortion.none
     _intrinsics.coeffs = [i for i in cameraInfo.D]
     result = rs.rs2_project_point_to_pixel(_intrinsics, point)
