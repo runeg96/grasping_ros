1fc340813e027a09995114345d47f5396e08b213
Rune Grønhøj <runeg96@hotmail.com>
2021-04-08 16:23:14 +0200

    Added dataset making script

--- src/grasp_lib/src/cv_img.py
+++ src/grasp_lib/src/cv_img.py
@@ -3,23 +3,43 @@ import rospy
 import cv2 as cv
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+import os
+import numpy as np
 
 rospy.init_node("cv_img")
 
 cvbridge = CvBridge()
 
-msg = rospy.wait_for_message("/ptu_camera/camera/aligned_depth_to_color/image_raw",Image,rospy.Duration(1))
-img = cvbridge.imgmsg_to_cv2(msg,desired_encoding="passthrough")
-print(img.dtype)
-cv.imwrite("Depth.png", img)
+here = os.path.dirname(os.path.abspath(__file__))
+index = 0
 
-msg_color = rospy.wait_for_message("/ptu_camera/camera/color/image_raw", Image, rospy.Duration(1))
-img_color = cvbridge.imgmsg_to_cv2(msg_color, desired_encoding="passthrough")
-print(img_color.dtype)
-cv.imwrite("Color.png", img_color)
+while not rospy.is_shutdown():
+    #wait for human input before taking new image
+    raw_input("press enter to continue")
+    
+    #wait for depth image
+    msg = rospy.wait_for_message("/ptu_camera/camera/aligned_depth_to_color/image_raw",Image,rospy.Duration(1))
+    img = cvbridge.imgmsg_to_cv2(msg,desired_encoding="passthrough")
 
-msg_depth = rospy.wait_for_message("/ptu_camera/camera/depth/image_rect_raw", Image, rospy.Duration(1))
-img_depth = cvbridge.imgmsg_to_cv2(msg_color, desired_encoding="passthrough")
-print(img_depth.dtype)
-cv.imwrite("Depth2.png", img_depth)
+    #save image in dataset folder
+    depth_name = os.path.join(here, "../dataset/depth_{}.png".format(index))
+    depth_name_tiff = os.path.join(here, "../dataset/depth_{}.tiff".format(index))
+
+    cv.imwrite(depth_name, img)
+    cv.imwrite(depth_name_tiff, img.astype(np.float32)/1000)
+
+    #wait for color image
+    msg_color = rospy.wait_for_message("/ptu_camera/camera/color/image_raw", Image, rospy.Duration(1))
+    img_color = cvbridge.imgmsg_to_cv2(msg_color, desired_encoding="passthrough")
+    
+    #convert stupid cv image to RGB
+    img_color = cv.cvtColor(img_color, cv.COLOR_BGR2RGB)
+
+    #Save color image in dataset folder
+    img_name = os.path.join(here, "../dataset/color_{}.png".format(index))
+    cv.imwrite(img_name, img_color)
+
+    #Count of images
+    index += 1
+
 
