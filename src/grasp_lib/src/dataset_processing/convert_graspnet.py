0e29392ba13f79b3f5c3807a9d8436d60211faa6
Rune <runeg96@hotmail.com>
2021-04-23 11:26:40 +0200

    Added new filtering for converting good rect labels

--- src/grasp_lib/src/dataset_processing/convert_graspnet.py
+++ src/grasp_lib/src/dataset_processing/convert_graspnet.py
@@ -2,20 +2,39 @@ import os
 import glob
 import sys
 import numpy as np
+from tqdm import tqdm
 
 file_path = "/home/slave/Documents/Datasets/Graspnet"
+TOTAL_SCENE_NUM = 190
+camera = "realsense"
+fric_coef_thresh = 0.4
+split = "train"
 
-test = False
-if test:
-    graspf = glob.glob(os.path.join(file_path, 'scene_01*', 'realsense/rect', '*.npy'))
-else:
-    graspf = glob.glob(os.path.join(file_path, 'scene_00*', 'realsense/rect', '*.npy'))
+sceneIds = []
+if split == 'all':
+    sceneIds = list(range(TOTAL_SCENE_NUM))
+elif split == 'train':
+    sceneIds = list(range(100))
+elif split == 'test':
+    sceneIds = list(range(100, 190))
+elif split == 'test_seen':
+    sceneIds = list(range(100, 130))
+elif split == 'test_similar':
+    sceneIds = list(range(130, 160))
+elif split == 'test_novel':
+    sceneIds = list(range(160, 190))
 
-for idx, file in enumerate(graspf):
+rectLabelPath = []
+for i in tqdm(sceneIds, desc='Loading data path...'):
+    for img_num in range(256):
+        rectLabelPath.append(os.path.join(file_path, 'scenes', 'scene_'+str(i).zfill(4), camera, 'rect', str(img_num).zfill(4)+'.npy'))
+
+for idx, file in enumerate(tqdm(rectLabelPath, desc='Converting data')):
     #Load data
     f = np.load(file)
-    print("Processing: ", file)
-    print("[",idx,"/",len(graspf),"]")
+    mask = f[:,5] >= (1.1 - fric_coef_thresh)
+    f = f[mask]
+
 
     # alocate room for data
     array = np.zeros((len(f),5))
