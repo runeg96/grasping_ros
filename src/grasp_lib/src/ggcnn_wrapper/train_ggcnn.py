e1917c225dcbf476cdfc7846226e88789383cc99
Rune Grønhøj <runeg96@hotmail.com>
2021-04-29 11:00:39 +0200

    Fixed validation

--- src/grasp_lib/src/ggcnn_wrapper/train_ggcnn.py
+++ src/grasp_lib/src/ggcnn_wrapper/train_ggcnn.py
@@ -81,37 +81,37 @@ def validate(net, device, val_data, batches_per_epoch):
 
     with torch.no_grad():
         batch_idx = 0
-        while batch_idx < batches_per_epoch:
-            for x, y, didx, rot, zoom_factor in val_data:
-                batch_idx += 1
-                if batches_per_epoch is not None and batch_idx >= batches_per_epoch:
-                    break
+        # while batch_idx < batches_per_epoch:
+        for x, y, didx, rot, zoom_factor in val_data:
+            batch_idx += 1
+            # if batches_per_epoch is not None and batch_idx >= batches_per_epoch:
+            #     break
 
-                xc = x.to(device)
-                yc = [yy.to(device) for yy in y]
-                lossd = net.compute_loss(xc, yc)
+            xc = x.to(device)
+            yc = [yy.to(device) for yy in y]
+            lossd = net.compute_loss(xc, yc)
 
-                loss = lossd['loss']
+            loss = lossd['loss']
 
-                results['loss'] += loss.item()/ld
-                for ln, l in lossd['losses'].items():
-                    if ln not in results['losses']:
-                        results['losses'][ln] = 0
-                    results['losses'][ln] += l.item()/ld
+            results['loss'] += loss.item()/ld
+            for ln, l in lossd['losses'].items():
+                if ln not in results['losses']:
+                    results['losses'][ln] = 0
+                results['losses'][ln] += l.item()/ld
 
-                q_out, ang_out, w_out = post_process_output(lossd['pred']['pos'], lossd['pred']['cos'],
-                                                            lossd['pred']['sin'], lossd['pred']['width'])
+            q_out, ang_out, w_out = post_process_output(lossd['pred']['pos'], lossd['pred']['cos'],
+                                                        lossd['pred']['sin'], lossd['pred']['width'])
 
-                s = evaluation.calculate_iou_match(q_out, ang_out,
-                                                   val_data.dataset.get_gtbb(didx, rot, zoom_factor),
-                                                   no_grasps=1,
-                                                   grasp_width=w_out,
-                                                   )
+            s = evaluation.calculate_iou_match(q_out, ang_out,
+                                                val_data.dataset.get_gtbb(didx, rot, zoom_factor),
+                                                no_grasps=1,
+                                                grasp_width=w_out,
+                                                )
 
-                if s:
-                    results['correct'] += 1
-                else:
-                    results['failed'] += 1
+            if s:
+                results['correct'] += 1
+            else:
+                results['failed'] += 1
 
     return results
 
