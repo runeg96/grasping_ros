9a68cd4a08b9fd65911859157ab3d117b244c33f
Rune Grønhøj <runeg96@hotmail.com>
2021-05-17 14:41:05 +0200

    Convert multi-view labels

--- src/grasp_lib/src/dataset_processing/txt_modifier.py
+++ src/grasp_lib/src/dataset_processing/txt_modifier.py
@@ -1,18 +1,19 @@
 
 import numpy as np
 
+views = ["side","head","top"]
+for view in views:
+    for i in range(50):
+        with open('/home/slave/Documents/workspaces/handover_ws/src/lh7-handover/grasping_ros/src/grasp_lib/dataset/{}_{}_color.txt'.format(i,view), 'a+') as file:
+            file.seek(0)
+            f = open("/home/slave/Documents/workspaces/handover_ws/src/lh7-handover/grasping_ros/src/grasp_lib/dataset/{}_{}_annotations.txt".format(i, view), "a")
+            
+            for line in file:
+                y = line.split()
+                arr = np.array(y)
+                arr = arr.reshape(-1, 2)
+                # print(arr)
+                for x in arr:
+                    f.write(str(x[0]) + " " + str(x[1]) + "\n") 
 
-for i in range(26):
-    with open('rect_labels/color_{}.txt'.format(i), 'a+') as file:
-        file.seek(0)
-        f = open("rect_labels/color_{}_newer_annotations.txt".format(i), "a")
-        
-        for line in file:
-            y = line.split()
-            arr = np.array(y)
-            arr = arr.reshape(-1, 2)
-            # print(arr)
-            for x in arr:
-                f.write(str(x[0]) + " " + str(x[1]) + "\n") 
-
-        f.close()
+            f.close()
