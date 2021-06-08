00bf6e569483597c505fec54b1d6d682c6eb55ed
Jan <jkja16@student.aau.dk>
2021-06-08 11:55:50 +0200

    Added argument passing to convert_graspnet

--- src/grasp_lib/src/dataset_processing/convert_graspnet.py
+++ src/grasp_lib/src/dataset_processing/convert_graspnet.py
@@ -1,18 +1,21 @@
 import os
-import glob
-import sys
+import argparse
 import numpy as np
 from tqdm import tqdm
 
-file_path = "/raid/Graspnet/Graspnet"
-TOTAL_SCENE_NUM = 190
-camera = "realsense"
-fric_coef_thresh = 0.4
-fric = str(fric_coef_thresh).replace('.','')
-# split = "all"
-splits = ["train", "test", "test_seen", "test_similar", "test_novel"]
 
-sceneIds = []
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert Graspnet dataset')
+
+    # Dataset
+    parser.add_argument('--dataset-path', type=str, default='/raid/Graspnet/Graspnet', help='Path to dataset')
+    parser.add_argument('--camera', type=str, default='realsense', help='Camera (realsense/kinect)')
+    parser.add_argument('--fric-coef', type=float, default=0.4, help='Friction coefficient for sorting data (0.0-1.0)')
+
+    args = parser.parse_args()
+
+    return args
+
 
 def cornell_format(center, angle, width=60, height=30):
     """
@@ -29,68 +32,73 @@ def cornell_format(center, angle, width=60, height=30):
 
     return np.array(
         [
-         [y1 - height/2 * xo, x1 - height/2 * yo],
-         [y2 - height/2 * xo, x2 - height/2 * yo],
-         [y2 + height/2 * xo, x2 + height/2 * yo],
-         [y1 + height/2 * xo, x1 + height/2 * yo],
-         ]
+            [y1 - height / 2 * xo, x1 - height / 2 * yo],
+            [y2 - height / 2 * xo, x2 - height / 2 * yo],
+            [y2 + height / 2 * xo, x2 + height / 2 * yo],
+            [y1 + height / 2 * xo, x1 + height / 2 * yo],
+        ]
     ).astype(np.float)
 
 
-for count, split in enumerate(splits):
-    print("Dataset: ", count,"/",len(splits))
-    # select what data to be converted
-    if split == 'all':
-        sceneIds = list(range(TOTAL_SCENE_NUM))
-    elif split == 'train':
-        sceneIds = list(range(100))
-    elif split == 'test':
-        sceneIds = list(range(100, 190))
-    elif split == 'test_seen':
-        sceneIds = list(range(100, 130))
-    elif split == 'test_similar':
-        sceneIds = list(range(130, 160))
-    elif split == 'test_novel':
-        sceneIds = list(range(160, 190))
+if __name__ == '__main__':
+    args = parse_args()
 
-    rectLabelPath = []
+    TOTAL_SCENE_NUM = 190
+    splits = ["train", "test", "test_seen", "test_similar", "test_novel"]
+    sceneIds = []
+    fric = str(args.fric_coef).replace('.', '')
 
-    # Find grasp file paths
-    for i in tqdm(sceneIds, desc='Loading data path split: {}'.format(split)):
-        for img_num in range(256):
-            rectLabelPath.append(os.path.join(file_path,'scene_'+str(i).zfill(4), camera, 'rect', str(img_num).zfill(4)+'.npy'))
+    for count, split in enumerate(splits):
+        print("Dataset: ", count, "/", len(splits))
+        # select what data to be converted
+        if split == 'train':
+            sceneIds = list(range(100))
+        elif split == 'test':
+            sceneIds = list(range(100, TOTAL_SCENE_NUM))
+        elif split == 'test_seen':
+            sceneIds = list(range(100, 130))
+        elif split == 'test_similar':
+            sceneIds = list(range(130, 160))
+        elif split == 'test_novel':
+            sceneIds = list(range(160, TOTAL_SCENE_NUM))
 
-    mean_array = np.zeros(len(rectLabelPath))
-    # Go though all files in scenes
-    for index, file in enumerate(tqdm(rectLabelPath, desc='Converting data split: {}'.format(split))):
+        rectLabelPath = []
 
-        array = []
+        # Find grasp file paths
+        for i in tqdm(sceneIds, desc='Loading data path split: {}'.format(split)):
+            for img_num in range(256):
+                rectLabelPath.append(
+                    os.path.join(args.dataset_path, 'scenes',  'scene_' + str(i).zfill(4), args.camera, 'rect', str(img_num).zfill(4) + '.npy'))
 
-        #Load data
-        f = np.load(file)
+        mean_array = np.zeros(len(rectLabelPath))
+        # Go though all files in scenes
+        for index, file in enumerate(tqdm(rectLabelPath, desc='Converting data split: {}'.format(split))):
 
-        #discard all grasps with a high friction value(Grasp force closure)
-        mask = f[:,5] >= (1.1 - fric_coef_thresh)
-        f = f[mask]
+            array = []
 
-        mean_array[index] = np.mean(f[:,0])
+            # Load data
+            f = np.load(file)
 
-        for l in f:
-            x, y, ox,oy ,h ,_ ,_ = l
-            if mean_array[index] - 360 < x < mean_array[index] + 360 and 0 < y < 720:
+            # Discard all grasps with a high friction value (Grasp force-closure)
+            mask = f[:, 5] >= (1.1 - args.fric_coef)
+            f = f[mask]
 
-                angle = -np.arctan2(oy-y, ox-x)
-                w = np.hypot(ox - x, oy - y) * 2
+            mean_array[index] = np.mean(f[:, 0])
 
-                # Insert data in temp array
-                array.append(cornell_format(np.array([y,x]),angle,w,h))
+            for l in f:
+                x, y, ox, oy, h, _, _ = l
+                if mean_array[index] - 360 < x < mean_array[index] + 360 and 0 < y < 720:
+                    angle = -np.arctan2(oy - y, ox - x)
+                    w = np.hypot(ox - x, oy - y) * 2
 
-        array = np.array(array)
+                    # Insert data in temp array
+                    array.append(cornell_format(np.array([y, x]), angle, w, h))
 
+            array = np.array(array)
 
-        #Save grasp data for each file
-        output_line = file[:-4] + '_fric' + fric + file[-4:]
-        np.save(output_line,array)
+            # Save grasp data for each file
+            output_line = file[:-4] + '_fric' + fric + file[-4:]
+            np.save(output_line, array)
 
-    #Save means for all files in root dir
-    np.save(file_path + "/"+ split + "_mean" + fric + ".npy", mean_array)
+        # Save means for all files in root dir
+        np.save(args.dataset_path + "/" + split + "_mean" + fric + ".npy", mean_array)
